fc5d778cd5dd96297a6a39d14fe65d7143c5b2af
Yeuda Shtern <yeudashtern@gmail.com>
2024-01-01 16:01:09 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
+})
+
+export const metadata: Metadata = {
+  title: 'Evently',
+  description: 'Evently us a platform made by Yehuda Shtern',
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { Separator } from "@radix-ui/react-separator"
+import Image from "next/image"
+import NavItems from "./NavItems"
+
+
+const MobileNav = () => {
+    return (
+        <nav className="md:hidden">
+            <Sheet>
+                <SheetTrigger className="align-middle">
+                    <Image
+                        src="/assets/icons/menu.svg"
+                        alt="manu"
+                        width={24}
+                        height={24}
+                        className="cursor-pointer"
+                    />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                    <Image
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width={128}
+                        height={38}
+                    />
+                    <Separator className="border border-gray-100" />
+                    <NavItems />
+
+                </SheetContent>
+            </Sheet>
+
+        </nav>
+    )
+}
+
+export default MobileNav
